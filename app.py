7fa36798219c41d3fe9494b14313b112bead0275
Ollie Power <powerollie@gmail.com>
2019-11-19 21:48:10 +0000

    Initial commit of Hello World app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+import os
+from flask import Flask
+
+app = Flask(__name__,
+            template_folder="templates")
+
+@app.route("/")
+def home():
+    return "<h1>Hello World</h1>"
+
+if __name__ == '__main__':
+    #app.run(host=os.environ.get('IP'),
+    #        port=int(os.environ.get('PORT')),
+    #        debug=True)
+    app.run(debug=True)
