30db0957a6cdaa9be9afd5028a615b73f50df26f
Ollie Power <powerollie@gmail.com>
2019-11-19 22:20:51 +0000

    Changed app run settings for Heroku

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def home():
     return "<h1>Hello World</h1>"
 
 if __name__ == '__main__':
-    #app.run(host=os.environ.get('IP'),
-    #        port=int(os.environ.get('PORT')),
-    #        debug=True)
-    app.run(debug=True)
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=False)
+    #app.run(debug=True)
